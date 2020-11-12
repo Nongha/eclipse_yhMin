505e0bee2bd3b5e5002be033acc3839be9039e59
Nongha <67034048+Nongha@users.noreply.github.com>
2020-11-12 11:52:04 +0900

    print Jeonbuk Iksan

--- Helloworld/src/Helloworld/Pushprint.java
+++ Helloworld/src/Helloworld/Pushprint.java
@@ -3,7 +3,8 @@ package Helloworld;
 public class Pushprint {
 
 	public static void main(String[] args) {
-		// System.out.println("sophomore");
+		System.out.println("sophomore");
+		System.out.println("Jeonbuk Iksan");
 	}
 
 }
